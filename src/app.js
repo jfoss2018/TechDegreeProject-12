7efd5950563e74498fad1280ac2fc84d9080d7e0
James Foss <jamesfoss1@hotmail.com>
2018-11-13 21:11:25 -0500

    Working Twitter API

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const Twit = require('twit');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
 const bodyParser = require('body-parser');
+const apiKeys = require('../.config.js');
 
 const mongodbURI = 'mongodb://someuser:password1234@ds161183.mlab.com:61183/heroku_ljvqjwlq';
 
@@ -18,13 +19,23 @@ db.once('open', function() {
   console.log('The db connection was successful.');
 });
 
-/*
 const T = new Twit({
-  consumer_key: ,
-  consumer_secret: ,
+  consumer_key: apiKeys.twitterConsumer,
+  consumer_secret: apiKeys.twitterSecret,
   app_only_auth: true
 });
-*/
+
+let params = {
+  q: "",
+  geocode: '31.2074,-83.2502,2mi',
+  count: 1
+};
+
+T.get('search/tweets', params, function(err, data, response) {
+  console.log(data);
+  console.log(data.statuses[0].user);
+});
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
